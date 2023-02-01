44ea7328875f59be60ae75f49fcd968c5d6f936d
NoxxFlame <noxxflame@gmail.com>
2023-02-01 12:13:13 +0000

    Added blindish and paranoia gifts

---
+++ lua/randomat2/secretsanta_choices/blindish.lua
@@ -0,0 +1,27 @@
+local CHOICE = {}
+
+util.AddNetworkString("RdmtSecretSantaBlindishBegin")
+util.AddNetworkString("RdmtSecretSantaBlindishEnd")
+
+CHOICE.Name = "Blind...ish"
+CHOICE.Id = "blindish"
+
+function CHOICE:Choose(owner, target)
+    target:ScreenFade(SCREENFADE.STAYOUT, Color(0,0,0,255),0, 0)
+
+    net.Start("RdmtSecretSantaBlindishBegin")
+    net.WriteString(target:SteamID64())
+    net.WriteString(owner:SteamID64())
+    net.Broadcast()
+end
+
+function CHOICE:CleanUp()
+    for _, p in ipairs(player.GetAll()) do
+        p:ScreenFade(SCREENFADE.PURGE, Color(0,0,0,255),0, 0)
+    end
+
+    net.Start("RdmtSecretSantaBlindishEnd")
+    net.Broadcast()
+end
+
+SECRETSANTA:RegisterChoice(CHOICE, true)
