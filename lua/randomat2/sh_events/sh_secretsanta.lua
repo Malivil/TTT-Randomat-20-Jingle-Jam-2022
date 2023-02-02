c829cb48a3bd73e3ab17d98a132bc837de9177c3
Malivil <mlebson@outlook.com>
2023-02-01 22:24:36 -0500

    Moved client and shared secret santa choice into their own folder. For #3

--- lua/randomat2/sh_events/sh_secretsanta.lua
+++ lua/randomat2/sh_events/sh_secretsanta.lua
@@ -1,7 +1,3 @@
-local string = string
-
-local StringStartsWith = string.StartsWith
-
 local function AddServer(fil)
     if SERVER then include(fil) end
 end
@@ -11,16 +7,18 @@ local function AddClient(fil)
     if CLIENT then include(fil) end
 end
 
-local files, _ = file.Find("randomat2/secretsanta_choices/*.lua", "LUA")
-for _, fil in ipairs(files) do
-    -- Files that start with "cl_" should be loaded on the client
-    if StringStartsWith(fil, "cl_") then
-        AddClient("randomat2/secretsanta_choices/" .. fil)
-    -- Files that start with "sh_" should be loaded on both the server and the client
-    elseif StringStartsWith(fil, "sh_") then
-        AddServer("randomat2/secretsanta_choices/" .. fil)
-        AddClient("randomat2/secretsanta_choices/" .. fil)
-    else
-        AddServer("randomat2/secretsanta_choices/" .. fil)
-    end
+local serverFiles, _ = file.Find("randomat2/secretsanta_choices/*.lua", "LUA")
+for _, fil in ipairs(serverFiles) do
+    AddServer("randomat2/secretsanta_choices/" .. fil)
+end
+
+local clientFiles, _ = file.Find("randomat2/secretsanta_choices/client/*.lua", "LUA")
+for _, fil in ipairs(clientFiles) do
+    AddClient("randomat2/secretsanta_choices/client/" .. fil)
+end
+
+local sharedFiles, _ = file.Find("randomat2/secretsanta_choices/shared/*.lua", "LUA")
+for _, fil in ipairs(sharedFiles) do
+    AddServer("randomat2/secretsanta_choices/shared/" .. fil)
+    AddClient("randomat2/secretsanta_choices/shared/" .. fil)
 end
