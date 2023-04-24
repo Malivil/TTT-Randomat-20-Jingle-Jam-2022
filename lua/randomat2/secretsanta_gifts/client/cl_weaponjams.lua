21ff175cf6c35943d728e65057c83ed5b677987e
Malivil <mlebson@outlook.com>
2023-04-24 09:22:47 -0400

    Added some error protection to the weapon jam secret santa gift

--- lua/randomat2/secretsanta_gifts/client/cl_weaponjams.lua
+++ lua/randomat2/secretsanta_gifts/client/cl_weaponjams.lua
@@ -33,7 +33,7 @@ local function UnjamWeapon(weap)
 end
 
 net.Receive("RdmtSecretSantaWeaponJamsStart", function()
-    if not client then
+    if not IsPlayer(client) then
         client = LocalPlayer()
     end
 
@@ -44,7 +44,7 @@ net.Receive("RdmtSecretSantaWeaponJamsStart", function()
 end)
 
 net.Receive("RdmtSecretSantaWeaponJamsStop", function()
-    if not client then
+    if not IsPlayer(client) then
         client = LocalPlayer()
     end
 
@@ -55,9 +55,14 @@ net.Receive("RdmtSecretSantaWeaponJamsStop", function()
 end)
 
 net.Receive("RdmtSecretSantaWeaponJamsEnd", function()
-    if not client then
+    if not IsPlayer(client) then
         client = LocalPlayer()
     end
+
+    -- If we still don't have a client it's because we're not loaded yet
+    -- This can happen because the Randomat "ends" all events during the Prep phase so if
+    -- a player is still loading at that point then `LocalPlayer` would return a NULL Entity
+    if not client or not IsPlayer(client) or not client.GetWeapons then return end
 
     for _, w in ipairs(client:GetWeapons()) do
         UnjamWeapon(w)
