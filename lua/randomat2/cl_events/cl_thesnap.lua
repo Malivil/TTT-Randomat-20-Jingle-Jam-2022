01f256639a71b2f9f6e84e2b1a0ba03eb72bf923
Malivil <mlebson@outlook.com>
2024-11-29 22:49:18 -0500

    Minor tweak

--- lua/randomat2/cl_events/cl_thesnap.lua
+++ lua/randomat2/cl_events/cl_thesnap.lua
@@ -10,16 +10,12 @@ function EVENT:Begin()
     local grey_min = 105
     local grey_max = 220
 
-    hook.Add("Think", "RdmtTheSnapThink", function()
+    self:AddHook("Think", function()
         local grey = MathRand(grey_min, grey_max)
         Randomat:HandleEntitySmoke(ents.GetAll(), client, function(v)
             return v:GetNWBool("RdmtTheSnapDissolve", false)
         end, Color(grey, grey, grey), nil, 1, 1)
     end)
-end
-
-function EVENT:End()
-    hook.Remove("Think", "RdmtTheSnapThink")
 end
 
 Randomat:register(EVENT)
